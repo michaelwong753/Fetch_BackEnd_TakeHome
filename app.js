c54bd07c3dd22c24a028c85ffa95ceb103fa8b1d
root <root@DESKTOP-51DOO84.localdomain>
2023-01-11 01:19:17 -0600

    Add some changes

--- app.js
+++ app.js
@@ -5,24 +5,35 @@ const Receipt = require('./receipt')
 const app = express()
 const port = 3000
 
-const receiptSchema = new Set(["retailer","purchaseDate","purchaseTime","items","total","shortDescription","price"])
+const receiptSchema = new Set(["retailer","purchaseDate","purchaseTime","items","total"])
+const itemSchema = new Set(["shortDescription","price"])
 var database = {}
 
+function isObjectValid(obj,schemaType){
+	let numOfValidKeys = 0
+	for (item in obj){
+		if (schemaType.has(item)) {
+			numOfValidKeys += 1
+			if (!isValueValid(item,obj[item])) return false
+		}
+	} 
+	if (numOfValidKeys != schemaType.size) return false
+	return true
+}
+
 function isValueValid(key,val){
-	if (!receiptSchema.has(key)) return false
-	else if (key == "retailer" || key == "shortDescription") return (val.length > 0)
+	if (key == "retailer" || key == "shortDescription") return (val.length > 0)
 	else if (key == "total" || key == "price") return /^\d+\.\d{2}$/.test(val)
 	else if (key == "purchaseTime") return moment(val, "HH:mm", true).isValid()
 	else if (key == "purchaseDate") return moment(val, "YYYY-MM-DD", true).isValid()
 	else if (key == "items"){
 		if (val.length < 1) return false
 		for (var i = 0; i < val.length; i++){
-			for (item in val[i]){
-				if (!isValueValid(item,val[i][item])) return false
-			} 
+			if (!isObjectValid(val[i],itemSchema)) return false
 		}
 		return true
 	}
+
 	return false
 }
 
@@ -31,12 +42,11 @@ app.use(bodyParser.json())
 
 app.post('/receipts/process', (req,res) =>{
 	var jsonReceipt = req.body
-	for (var key in jsonReceipt){
-		if (!isValueValid(key,jsonReceipt[key])){
-			res.status(400).send("The receipt is invalid")
-			return
-		}
+	if (!isObjectValid(jsonReceipt,receiptSchema)){
+		res.status(400).send("The receipt is invalid")
+		return
 	}
+
 	var receipt = new Receipt(jsonReceipt)
 	database[receipt.id] = receipt
 	res.send({"id": receipt.id})
